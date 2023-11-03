9b64d0e26e51a354cc970d090e3e7905cda194fa
rjezra <jahoely@gmail.com>
2023-11-03 13:14:57 +0300

    exojs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
+/*
 var btn = document.getElementById("button");
-
 btn.onclick = function () {
     var division = parseFloat(diviseur.value);
     var valeur1 = parseFloat(numbre1.value);
@@ -25,4 +25,37 @@ btn.onclick = function () {
 
     document.getElementById("resulat").innerHTML = resultatText;
 }
+*/
+var btn = document.getElementById("button");
 
+btn.onclick = function () {
+    var diviseur = document.getElementById("diviseur");
+    var numb1 = document.getElementById("numbre1");
+    var numb2 = document.getElementById("numbre2");
+
+    var division = parseFloat(diviseur.value);
+    var valeur1 = parseFloat(numb1.value);
+    var valeur2 = parseFloat(numb2.value);
+
+    var resultatText = "";
+
+    if (isNaN(division) || isNaN(valeur1) || isNaN(valeur2)) {
+        resultatText = "Veuillez entrer des nombres valides.";
+    } else if (division != 0 ) {
+        function afficherDiviseurs(divisions) {
+            var diviseurs = [];
+            for (var i = valeur1; i <= valeur2; i++) {
+                if (i % divisions === 0) {
+                    diviseurs.push(i);
+                }
+            }
+            return diviseurs;
+        }
+
+        var diviseurs = afficherDiviseurs(division);
+        resultatText = "Les nombres divisibles par " + division + " entre " + valeur1 + " et " + valeur2 + " sont : " + diviseurs.join(", ");
+    } else {
+        resultatText = "FI";
+    }
+    document.getElementById("resulat").innerHTML = resultatText;
+}
